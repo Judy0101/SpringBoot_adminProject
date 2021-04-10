5a8f6cbad5a90082f2dde6927f061baf92d8858b
Haeji <haillydev@gmail.com>
2021-04-11 01:47:41 +0900

    Create Repository

---
+++ src/main/java/com/example/myfirst/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.myfirst.repository;
+
+import org.junit.experimental.categories.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
