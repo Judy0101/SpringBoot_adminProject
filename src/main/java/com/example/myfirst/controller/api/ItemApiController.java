c0ff1a607e09e6790f815c03b61084e68786af6f
Haeji <haillydev@gmail.com>
2021-04-12 00:36:50 +0900

    Add Item Delete API

--- src/main/java/com/example/myfirst/controller/api/ItemApiController.java
+++ src/main/java/com/example/myfirst/controller/api/ItemApiController.java
@@ -40,6 +40,6 @@ public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiR
     @Override
     @DeleteMapping("{id}")
     public Header delete(@PathVariable Long id) {
-        return null;
+        return itemApiLogicService.delete(id);
     }
 }
