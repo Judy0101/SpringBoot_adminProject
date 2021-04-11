12960929d1c2172ccc249d663c91d50e8e022964
Haeji <haillydev@gmail.com>
2021-04-11 17:05:53 +0900

    Define CRUD Interface and Write Common ResponseBody

--- src/main/java/com/example/myfirst/controller/GetController.java
+++ src/main/java/com/example/myfirst/controller/GetController.java
@@ -1,6 +1,7 @@
 package com.example.myfirst.controller;
 
 import com.example.myfirst.model.SearchParam;
+import com.example.myfirst.model.network.Header;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -31,5 +32,12 @@ public class GetController {
         System.out.println(searchParam.getPage());
 
         return searchParam;
+    }
+
+    @GetMapping("/header")
+    public Header getHeader(){
+
+        // {"resultCode" :"OK", "descrition" : "OK"}
+        return Header.builder().resultCode("OK").description("OK").build();
     }
 }
