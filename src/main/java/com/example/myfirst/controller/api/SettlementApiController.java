603507a675c14f90c86f9e4f99f91057558ed4e0
Haeji <haillydev@gmail.com>
2021-04-12 09:38:40 +0900

    Refactoring SettlementController

--- src/main/java/com/example/myfirst/controller/api/SettlementApiController.java
+++ src/main/java/com/example/myfirst/controller/api/SettlementApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 
-/* Create Controller
+/* Create SQL
 CREATE TABLE `settlement` (
   `user_id` bigint NOT NULL,
   `price` decimal(12,4) NOT NULL,
